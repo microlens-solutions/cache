c11ff28abfdf5df4b5854a018cf7380d0212b35b
Muhammad Mansoor Afzal <mansoorafzal@gmail.com>
2026-08-15 15:13:43 +0400

    Add project files.

---
+++ Microlens.Cache/Extensions/ServiceCollectionExtensions.cs
@@ -0,0 +1,18 @@
+﻿using Microlens.Cache.Services;
+using Microsoft.Extensions.DependencyInjection;
+using Unity;
+using Unity.Lifetime;
+
+namespace Microlens.Cache.Extensions;
+
+public static class ServiceCollectionExtensions {
+    public static IServiceCollection AddMicrolensCache(this IServiceCollection services) {
+        _ = services.AddSingleton<ICachingService, CachingService>();
+        return services;
+    }
+
+    public static IUnityContainer AddMicrolensCache(this IUnityContainer container) {
+        _ = container.RegisterType<ICachingService, CachingService>(new ContainerControlledLifetimeManager());
+        return container;
+    }
+}
---
+++ Microlens.Cache/Services/ICachingService.cs
@@ -0,0 +1,9 @@
+﻿using System.Collections.Concurrent;
+
+namespace Microlens.Cache.Services;
+
+public interface ICachingService {
+    Task<TValue> GetOrAddAsync<TValue>(string container, string key, Func<Task<TValue>> factory, bool rebuild = false);
+
+    Task<TValue> GetOrAddAsync<TKey, TValue>(string container, string collection, TKey key, Func<Task<ConcurrentDictionary<TKey, TValue>>> factory, bool refresh = false) where TKey : notnull;
+}
